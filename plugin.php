75ba2c07482879487cdcf4aeae65a6524ec4318c
Stephen Dickinson <stephencottontail@me.com>
2025-08-31 09:16:29 -0600

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Personal Site Blocks
+ * Author: Stephen Dickinson <stephencottontail@me.com>
+ * Description: A collection of blocks for my personal site
+ * Version: 1.0.0
+ *
+ * @package personal-site-blocks
+ */
+
+if ( ! defined( 'PSB_PLUGIN_DIR' ) ) {
+	define( 'PSB_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+if ( ! defined( 'PSB_PLUGIN_URL' ) ) {
+	define( 'PSB_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
+if ( ! defined( 'PSB_VERSION' ) ) {
+	define( 'PSB_VERSION', '1.0.0' );
+}
