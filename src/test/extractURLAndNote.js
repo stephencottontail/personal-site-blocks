f3b1c8f6ea75ac718c3a8999be02fd7062a27a08
Stephen Dickinson <stephencottontail@me.com>
2025-09-03 17:21:07 -0600

    Test extracting URL from list block

---
+++ src/test/extractURLAndNote.js
@@ -0,0 +1,13 @@
+import { describe, expect, toEqual } from '@jest/globals';
+import extractURLAndNote from '../utils/extractURLAndNote.js';
+
+test( 'Extract URL and an optional note from a block', () => {
+	const content = '<a href="https://www.apple.com/">https://www.apple.com/</a> (Apple)';
+	const got = extractURLAndNote(content);
+	const want = {
+		link: 'https://www.apple.com/',
+		note: 'Apple',
+	};
+
+	expect( got ).toEqual( want );
+} );
---
+++ src/utils/extractURLAndNote.js
@@ -0,0 +1,8 @@
+const extractURLAndNote = ( content ) => {
+	return {
+		link: 'https://www.apple.com/',
+		note: 'Apple',
+	};
+};
+
+export default extractURLAndNote;
