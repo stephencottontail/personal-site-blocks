a3e0d691987896af13285367ad7b4a7ff81ddb7d
Stephen Dickinson <stephencottontail@me.com>
2025-09-03 17:28:33 -0600

    No nood to explicitly import from `jest`

--- src/test/extractURLAndNote.js
+++ src/test/extractURLAndNote.js
@@ -1,9 +1,9 @@
-import { describe, expect, toEqual } from '@jest/globals';
 import extractURLAndNote from '../utils/extractURLAndNote.js';
 
 test( 'Extract URL and an optional note from a block', () => {
-	const content = '<a href="https://www.apple.com/">https://www.apple.com/</a> (Apple)';
-	const got = extractURLAndNote(content);
+	const content =
+		'<a href="https://www.apple.com/">https://www.apple.com/</a> (Apple)';
+	const got = extractURLAndNote( content );
 	const want = {
 		link: 'https://www.apple.com/',
 		note: 'Apple',
