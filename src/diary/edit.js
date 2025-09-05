65c03542cd5efc5fb5848746c57c06cd575b36c8
Stephen Dickinson <stephencottontail@me.com>
2025-09-05 17:54:29 -0600

    Add date to block

--- src/diary/edit.js
+++ src/diary/edit.js
@@ -1,11 +1,40 @@
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
 
 const Edit = ( props ) => {
-	const { attributes, setAttributes } = props;
+	const { attributes, setAttributes, clientId } = props;
+	const { date } = attributes;
 	const blockProps = useBlockProps();
+	const formatDateForDisplay = ( date ) => {
+		const dateObject = new Date( date );
+		const options = {
+			weekday: 'long',
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+		};
+		return dateObject.toLocaleDateString( 'en-US', options );
+	};
+
+	const innerBlockCount = useSelect(
+		( select ) => {
+			const block = select( 'core/block-editor' ).getBlock( clientId );
+
+			return block ? block.innerBlocks.length : 0;
+		},
+		[ clientId ]
+	);
+
+	useEffect( () => {
+		const now = Date.now();
+
+		setAttributes( { date: now } );
+	}, [ innerBlockCount, clientId ] );
 
 	return (
 		<div { ...blockProps }>
+			{ date && <p>{ formatDateForDisplay( date ) }</p> }
 			<InnerBlocks
 				defaultBlock={ [
 					'core/paragraph',
